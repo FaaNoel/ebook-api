77279be5e1e3b73f8443f353135ed1c8ea1f3b45
Elfando Pratama <elfando.p22@gmail.com>
2020-08-19 21:51:22 +0700

    migration

---
+++ routes/api.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/me', 'AuthController@me');
+
+Route::get('/books', 'BookController@index');
+
+
+Route::post('/books/create', 'BookController@store');
+
+
+Route::post('/books/update/{id}', 'BookController@update');
+
+Route::delete('/books/delete/{id}', 'BookController@destroy');
